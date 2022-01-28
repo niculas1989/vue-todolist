94c05e16929418456b13c7b94c3eda1f16d1940f
niculas1989 <nicolas.maranzano@libero.it>
2022-01-28 14:52:52 +0100

    added "text" in Vue

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ const root = new Vue({
     el: '#root',
     data: {
         listElements: [
-            { text: '', done: false },
-            { text: '', done: false },
-            { text: '', done: false },
-            { text: '', done: false },
-            { text: '', done: false },
-            { text: '', done: false },
+            { text: 'Svegliarsi presto', done: false },
+            { text: 'Accendere almeno 15 minuti prima il PC', done: false },
+            { text: 'Seguire attentamente', done: false },
+            { text: 'Prendere appunti', done: false },
+            { text: 'Fare le esercitazioni pomeridiane', done: false },
+            { text: 'Svagarsi dopo', done: false },
         ]
     },
     methods: {
